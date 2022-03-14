fcd5ccb45ece66f292e6188eaf3fbb2332f4e003
nurit0105 <laura.davidowicz@stud.fh-campuswien.ac.at>
2022-03-14 20:49:09 +0100

    1

--- src/test/java/PasswordCtrlTest.java
+++ src/test/java/PasswordCtrlTest.java
@@ -1,49 +1,49 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class PasswordCtrlTest {
+// https://github.com/nurit0105/ue1-prog2.git
+
+class PasswordCtrlTest {
 
     @Test
     void checkPassword_too_short(){
-        boolean actual = PasswordCtrl.checkPassword("1234567");
+        boolean actual = PasswordCtrl.checkPasswordLength("1234567");
         boolean expected = false;
         Assertions.assertEquals(expected,actual);
     }
 
     @Test
     void checkPassword_correct_length(){
-        boolean acutal = PasswordCtrl.checkPassword("1234567890");
+        boolean acutal = PasswordCtrl.checkPasswordLength("1234567890");
         boolean expected = true;
         Assertions.assertEquals(expected, acutal);
     }
 
     @Test
     void checkPassword_too_long(){
-        boolean actual = PasswordCtrl.checkPassword("12345678901234567890123456");
+        boolean actual = PasswordCtrl.checkPasswordLength("12345678901234567890123456");
         boolean expected = false;
         Assertions.assertEquals(expected,actual);
     }
 
     @Test
-    void checkPassword_has_Lower_Characters(){
-        boolean actual = PasswordCtrl.checkLower_Upper_Character_and_Numbers("abcdefghijk");
-        boolean expected = true;
+    void checkPassword_has_noLower_Characters(){
+        boolean actual = PasswordCtrl.checkLower_Upper_Character_and_Numbers("ABCDEFGHIJ2!");
+        boolean expected = false;
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void checkPassword_has_Upper_Characters(){
-        boolean actual = PasswordCtrl.checkLower_Upper_Character_and_Numbers("ABCDEFGHIJK");
-        boolean expected = true;
+    void checkPassword_has_noUpper_Characters(){
+        boolean actual = PasswordCtrl.checkLower_Upper_Character_and_Numbers("abcdeghij2!");
+        boolean expected = false;
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void checkPassword_has_Numbers(){
-        boolean actual = PasswordCtrl.checkLower_Upper_Character_and_Numbers("1234567890");
-        boolean expected = true;
+    void checkPassword_has_noNumbers(){
+        boolean actual = PasswordCtrl.checkLower_Upper_Character_and_Numbers("bcdebcdebcdebcde");
+        boolean expected = false;
         Assertions.assertEquals(expected, actual);
     }
-
-
 }
